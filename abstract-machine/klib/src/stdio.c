b23ffb5261775a721ca0aceee8a190d2d2c5818b
tom xh wang <xwang397@jh.edu>
2023-12-11 10:53:55 -0500

    implement string and printf function

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,60 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+/// @brief fill the dst buffer with digits of int, in backward fashion
+/// @param dst destination buffer.
+/// @param d integer to be converted.
+/// @return the number of digits of the int `d`.
+int int_to_backstr(char* dst,int d) {
+  int cnt=0;
+  while (d) {
+    *dst++ = (char)(d%10+(int)'0'); 
+    d/=10; cnt++;
+  }
+  return cnt;
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  int d,cnt=0;
+  char c;
+  char *s;
+
+  va_start(ap, fmt);
+  while (*fmt) {
+    if(*fmt=='%') {
+      switch (*++fmt) {
+        case 's':              /* string */
+            s = va_arg(ap, char *);
+            int len_s = strlen(s);
+            out = strcpy(out,s) + len_s;cnt+=len_s;
+            break;
+        case 'd':              /* int */
+            d = va_arg(ap, int);
+            char int_str[10+2];
+            int int_num = int_to_backstr(int_str,d);
+            while (int_num-->0)
+            {
+              *out++ = int_str[int_num];
+              cnt++;
+            }
+            break;
+        case 'c':              /* char */
+            /* need a cast here since va_arg only
+              takes fully promoted types */
+            c = (char) va_arg(ap, int);
+            memcpy(out,&c,1); out++;cnt++;
+            break;
+      }
+      fmt++;
+    }
+    else {
+      *out++ = *fmt++;cnt++;
+    }
+  }
+  *out = '\0';
+  va_end(ap);
+  return cnt;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,97 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+//excluding '\0'
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0; 
+  while(*s!='\0'){
+    len++;s++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dptr = dst;
+  *dptr = *src;
+  do {
+    src++; dptr++;
+    *dptr = *src;
+  } while(*src!='\0');
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *dptr = dst;
+  while(n-- > 0) {
+    if(*src!='\0') {
+      *dptr = *src;
+      src++; dptr++;
+    }
+    else {
+      *dptr = '\0'; dptr++;
+    }
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dptr = dst;
+  while(*dst != '\0') dst++; //move to null terminator
+  strcpy(dst,src);
+  return dptr;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int cmp = 0;
+  while(cmp == 0 && !(*s1 == '\0' && *s2 == '\0')) {
+    cmp = *s1 - *s2;
+    s1++; s2++;
+  }
+  return cmp;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  int cmp = 0;
+  while(n-- >0 && cmp == 0 && !(*s1 == '\0' && *s2 == '\0')) {
+    cmp = *s1 - *s2;
+    s1++; s2++;
+  }
+  return cmp;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  void* sptr = s;
+  while(n-->0){
+    *(char*)s = (char) c;
+    // *(char*)s |= c << 12;
+    s++;
+  }
+  return sptr;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  //copy the src to heap, stored temporately. Copying it back to dst and free mem on heap. BUT here, just use extra space on the outer computer.
+  char temp[n+4];
+  memcpy(temp,src,n);
+  return memcpy(dst,temp,n);
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *dptr = out;
+  while(n-- > 0) {
+    *dptr = *(char*)in;
+    in++; dptr++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  int cmp = 0;
+  while(n-- >0 && cmp == 0) {
+    cmp = *(unsigned char*)s1 - *(unsigned char*)s2;
+    s1++; s2++;
+  }
+  return cmp;
 }
 
 #endif
